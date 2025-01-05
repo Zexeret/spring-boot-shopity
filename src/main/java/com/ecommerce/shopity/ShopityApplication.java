4a0e03aee254c1adcc275ce656bf1e4c22318e8e
Shyam Rathod <shyam.rathod2601@gmail.com>
2025-01-05 17:36:18 +0530

    feat: initial commit

---
+++ src/main/java/com/ecommerce/shopity/ShopityApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.shopity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/shopity/ShopityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.shopity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
